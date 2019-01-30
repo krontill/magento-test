71fe8e817b72ae62a700090b5fcf64bd143a42f0
s.shcherba <s.shcherba@itransition.com>
2019-01-30 20:52:55 +0300

    Add webpack and add script in footer.

---
+++ skin/frontend/new-rwd/default/src/sliders.js
@@ -0,0 +1,9 @@
+import { tns } from "../node_modules/tiny-slider/src/tiny-slider";
+
+const related = document.querySelector('.related-slider');
+
+if(related) {
+    const slider = tns({
+        container: '.related-slider',
+    });
+}
