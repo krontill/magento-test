a017b187d20711cefb81a57bcee624a55ae103dc
s.shcherba <s.shcherba@itransition.com>
2019-02-04 19:00:59 +0300

    Add radio for change color and font fot text over image.

--- skin/frontend/new-rwd/default/src/modification.js
+++ skin/frontend/new-rwd/default/src/modification.js
@@ -1,26 +1,58 @@
 const input = document.querySelector('[data-count]');
+const textImg = document.querySelector('.text-image');
+const radioColor = document.querySelector('[data-radio="color"]');
+const radioFont = document.querySelector('[data-radio="font"]');
 
-if (input) {
-
-    const handleEvent = () => {
+if (input && textImg) {
+    const handleInput = () => {
         const textLength = input.textLength;
         const textContent = input.value;
         const maxLengt = input.getAttribute('maxlength');
         const selector = input.getAttribute('data-count');
-        const textImg = input.getAttribute('data-text-img');
         counter(selector, textLength, maxLengt);
-        setTextImage(textImg, textContent);
+        setTextImage(textContent);
     };
 
     const counter = (selector, length, maxLengt) => {
         document.querySelector(selector).textContent = `${length}/${maxLengt}`;
     };
 
-    const setTextImage = (textImg, textContent) => {
-        document.querySelector(textImg).textContent = textContent;
+    const setTextImage = (textContent) => {
+        textImg.textContent = textContent;
     };
 
-    handleEvent();
+    handleInput();
 
-    input.oncut = input.oncopy = input.onpaste = input.oninput = handleEvent;
+    input.oncut = input.oncopy = input.onpaste = input.oninput = handleInput;
+}
+
+if (radioColor && textImg) {
+    const changeColor = (color) => {
+        textImg.className = 'text-image';
+        textImg.classList.add(`text-image--${color}`);
+    };
+
+    const colors = Array.from(radioColor.querySelectorAll('input[name="color"]'));
+
+    radioColor.addEventListener('change', (ev) => {
+        const elm = ev.target;
+        if (colors.includes(elm)) {
+            changeColor(elm.id);
+        }
+    }, false);
+}
+
+if (radioFont && textImg) {
+    const changeFont = (font) => {
+        textImg.setAttribute('data-font', font);
+    };
+
+    const fonts = Array.from(radioFont.querySelectorAll('input[name="font"]'));
+
+    radioFont.addEventListener('change', (ev) => {
+        const elm = ev.target;
+        if (fonts.includes(elm)) {
+            changeFont(elm.id);
+        }
+    }, false);
 }
